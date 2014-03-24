52bc15241d9f68121216da8408df11cb9e1927ac
Natalia Zarawska <nzarawska@gmail.com>
2014-03-24 14:41:18 +0100

    * added main manu

---
+++ Examples/BLEDemo/BLEDemo/code/NZMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  NZMenuViewController.h
+//  BLEDemo
+//
+//  Created by Natalia Zarawska on 24/03/14.
+//  Copyright (c) 2014 TUM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NZMenuViewController : UIViewController
+
+@end
---
+++ Examples/BLEDemo/BLEDemo/code/NZMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  NZMenuViewController.m
+//  BLEDemo
+//
+//  Created by Natalia Zarawska on 24/03/14.
+//  Copyright (c) 2014 TUM. All rights reserved.
+//
+
+#import "NZMenuViewController.h"
+
+@interface NZMenuViewController ()
+
+@end
+
+@implementation NZMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
